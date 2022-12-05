35e8c9de72fbe8fea387e55947f1c3c98123ffac
theodotcom <61735898+theodotcom@users.noreply.github.com>
2022-12-05 14:22:33 +0100

    firt step :construction_worker:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Index from './pages/Index';
+import Signin from './pages/Signin';
+import User from './pages/User';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/index" element={<Index />}></Route>
+          <Route exact path="/signin" element={<Signin />}></Route>
+          <Route exact path="/user" element={<User />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Signin.js
@@ -0,0 +1,57 @@
+import { Link } from 'react-router-dom'
+
+import logo from '../img/argentBankLogo.png'
+
+function Signin() {
+    return (
+        <div className="body_container">
+            <nav className="main-nav">
+                <Link className="main-nav-logo" to="/index">
+                    <img
+                        className="main-nav-logo-image"
+                        src={logo}
+                        alt="Argent Bank Logo"
+                    />
+                    <h1 className="sr-only">Argent Bank</h1>
+                </Link>
+                <div>
+                    <Link className="main-nav-item" to="/signin">
+                        <i className="fa fa-user-circle"></i>
+                        Sign In
+                    </Link>
+                </div>
+            </nav>
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
+                    <h1>Sign In</h1>
+                    <form>
+                        <div className="input-wrapper">
+                            <label for="username">Username</label>
+                            <input type="text" id="username" />
+                        </div>
+                        <div className="input-wrapper">
+                            <label for="password">Password</label>
+                            <input type="password" id="password" />
+                        </div>
+                        <div className="input-remember">
+                            <input type="checkbox" id="remember-me" />
+                            <label for="remember-me">Remember me</label>
+                        </div>
+
+                        <Link href="/user" className="sign-in-button">
+                            Sign In
+                        </Link>
+
+                        <button className="sign-in-button">Sign In</button>
+                    </form>
+                </section>
+            </main>
+            <footer className="footer">
+                <p className="footer-text">Copyright 2020 Argent Bank</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Signin
